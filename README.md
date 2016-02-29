dd7446bfbbdf957e9d3560a7998cfc18596c643f
Harriet Pulford <hatty@Harriets-MacBook-Pro.local>
2016-02-29 23:20:27 +0000

    fix(*) repo moved

---
+++ README.md
@@ -0,0 +1,4 @@
+cards
+=====
+
+Cards against humanity generator for creating custom cards. Currently parses CSVs.
---
+++ js/cardController.js
@@ -0,0 +1,29 @@
+var App = angular.module('App', []);
+
+App.controller('cardController', function($scope, $http) {
+	$http({
+		method: 'GET',
+		url: "./data/black.csv",
+		transformResponse: function(cards) {
+		// Transform CSV file into a JSON object
+		var json = $.csv.toObjects(cards);
+		return json;
+	},
+		cache: true,
+	})
+    .success(function(cards, status) {
+        $scope.cards = cards;
+    })
+    .error(function(data, status) {
+      $scope.cards = cards || "Request failed";
+    });
+
+    $scope.orderProp = 'model';
+});
+
+// function cardController($scope,$http) {
+//     $http.get("cards.json")
+//     .success(function(response) {
+//     	$scope.cards = response;
+//     });
+// }
